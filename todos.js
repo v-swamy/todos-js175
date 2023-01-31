3e6a041c4afe907e5ccdb46cb4239d4217a9ce3e
Vikram Swamy <vswamy@gmail.com>
2023-01-31 16:10:15 -0500

    added validation

--- todos.js
+++ todos.js
@@ -1,20 +1,105 @@
 const express = require("express");
 const morgan = require("morgan");
+const flash = require("express-flash");
+const session = require("express-session");
+const { body, validationResult } = require("express-validator");
+const TodoList = require("./lib/todolist");
 
 const app = express();
 const host = "localhost";
 const port = 3000;
 
+let todoLists = require("./lib/seed-data");
+
+
 app.set("views", "./views");
 app.set("view engine", "pug");
 
 app.use(morgan("common"));
-
-app.get("/", (req, res) => {
-  res.render("lists");
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+app.use(session({
+  name: "launch-school-todos-session-id",
+  resave: false,
+  saveUninitialized: true,
+  secret: "this is not very secure",
+}));
+app.use(flash());
+app.use((req, res, next) => {
+  res.locals.flash = req.session.flash;
+  delete req.session.flash;
+  next();
 });
 
-//Listener
+const sortByTitle = lists => {
+  return lists.slice().sort((todoListA, todoListB) => {
+    let titleA = todoListA.title.toLowerCase();
+    let titleB = todoListB.title.toLowerCase();
+
+    if (titleA < titleB) {
+      return -1;
+    } else if (titleA > titleB) {
+      return 1;
+    } else {
+      return 0;
+    }
+  });
+};
+
+const sortTodoLists = lists => {
+  let doneLists = lists.filter(list => list.isDone());
+  let undoneLists = lists.filter(list => !list.isDone());
+  return [ ...sortByTitle(undoneLists), ...sortByTitle(doneLists)];
+};
+
+app.get("/", (req, res) => {
+  res.redirect("/lists");
+});
+
+// Render the list of todo lists
+app.get("/lists", (req, res) => {
+  res.render("lists", {
+    todoLists: sortTodoLists(todoLists),
+  });
+});
+
+// Render new todo list page
+app.get("/lists/new", (req, res) => {
+  res.render("new-list");
+});
+
+// Create a new todo list
+app.post("/lists",
+  [
+    body("todoListTitle")
+      .trim()
+      .isLength({ min: 1 })
+      .withMessage("The list title is required")
+      .isLength({ max: 100 })
+      .withMessage("List title must be between 1 and 100 characters.")
+      .custom(title => {
+        let duplicate = todoLists.find(list => list.title === title);
+        return duplicate === undefined;
+      })
+      .withMessage("List title must be unique."),
+  ],
+  (req, res) => {
+    let errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      errors.array().forEach(message => req.flash("error", message.msg));
+      res.render("new-list", {
+        flash: req.flash(),
+        todoListTitle: req.body.todoListTitle,
+      });
+    } else {
+      todoLists.push(new TodoList(req.body.todoListTitle));
+      req.flash("success", "The todo list has been created.");
+      res.redirect("/lists");
+    }
+  }
+);
+
+// Listener
 app.listen(port, host, () => {
   console.log(`Todos is listening on port ${port} of ${host}!`);
 });
